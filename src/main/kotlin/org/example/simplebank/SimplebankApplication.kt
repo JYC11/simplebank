a5390ef848c5dc4e76fffc1b204aefe97c4db2dc
JYC11 <jaeyoon.c@gmail.com>
2025-10-14 20:23:46 +0900

    initial commit

---
+++ src/main/kotlin/org/example/simplebank/SimplebankApplication.kt
@@ -0,0 +1,11 @@
+package org.example.simplebank
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SimplebankApplication
+
+fun main(args: Array<String>) {
+    runApplication<SimplebankApplication>(*args)
+}
