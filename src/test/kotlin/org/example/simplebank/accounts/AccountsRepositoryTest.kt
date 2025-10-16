cf8f90fd4ed41a2aea4f102e735d70c42e16f024
JYC11 <jaeyoon.c@gmail.com>
2025-10-16 22:43:42 +0900

    formatting

--- src/test/kotlin/org/example/simplebank/accounts/AccountsRepositoryTest.kt
+++ src/test/kotlin/org/example/simplebank/accounts/AccountsRepositoryTest.kt
@@ -1,11 +1,10 @@
 package org.example.simplebank.accounts
 
 import org.assertj.core.api.Assertions.assertThat
-import org.example.simplebank.utils.BaseIntegTest
 import org.example.simplebank.account.infra.AccountsJooqCrudRepository
 import org.example.simplebank.account.infra.AccountsRepository
 import org.example.simplebank.account.infra.AccountsRepositoryImpl
-import org.jooq.DSLContext
+import org.example.simplebank.utils.BaseIntegTest
 import org.jooq.generated.tables.pojos.JAccounts
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
