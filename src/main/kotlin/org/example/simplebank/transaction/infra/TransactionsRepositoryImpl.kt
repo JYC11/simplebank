267e07eab284a563c26630b0512e2918ab8c0d9f
JYC11 <jaeyoon.c@gmail.com>
2025-10-16 20:38:40 +0900

    disallow modifying transactions

--- src/main/kotlin/org/example/simplebank/transaction/infra/TransactionsRepositoryImpl.kt
+++ src/main/kotlin/org/example/simplebank/transaction/infra/TransactionsRepositoryImpl.kt
@@ -12,13 +12,13 @@ class TransactionsRepositoryImpl(
 ) : TransactionsRepository {
     override fun getById(id: UUID) = jooqCrudRepository.getById(id)
     override fun save(entity: JTransactions) = jooqCrudRepository.save(entity)
-    override fun update(entity: JTransactions) = jooqCrudRepository.update(entity)
-    override fun delete(entity: JTransactions) = jooqCrudRepository.delete(entity)
-    override fun deleteById(id: UUID) = jooqCrudRepository.deleteById(id)
+    override fun update(entity: JTransactions) = throw UnsupportedOperationException()
+    override fun delete(entity: JTransactions) = throw UnsupportedOperationException()
+    override fun deleteById(id: UUID) = throw UnsupportedOperationException()
     override fun existsById(id: UUID) = jooqCrudRepository.existsById(id)
     override fun saveAll(entities: Collection<JTransactions>) = jooqCrudRepository.saveAll(entities)
-    override fun updateAll(entities: Collection<JTransactions>) = jooqCrudRepository.updateAll(entities)
-    override fun deleteAll(entities: Collection<JTransactions>) = jooqCrudRepository.deleteAll(entities)
+    override fun updateAll(entities: Collection<JTransactions>) = throw UnsupportedOperationException()
+    override fun deleteAll(entities: Collection<JTransactions>) = throw UnsupportedOperationException()
 
     override fun paginate(pageable: PaginationRequest): PaginationResponse<TransactionsPageRes> {
         TODO("Not yet implemented")
